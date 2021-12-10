d04bdf5af404aef6bd65191a96afd4b0fdaa8855
roman-developer <jose.maria.roman87@gmail.com>
2021-12-10 12:10:23 +0100

    [5] about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,5 +1,6 @@
 import * as React from "react"
 import { Link } from "gatsby"
+import { StaticImage } from "gatsby-plugin-image"
 
 import Layout from "../components/layout"
 import Seo from "../components/seo"
@@ -7,19 +8,31 @@ import Seo from "../components/seo"
 const About = () => (
   <Layout>
     <Seo title="About" />
-    <h1>About me</h1>
-    
+    <h1>Sobre mí</h1>
     <p>
-      Currently, I am remotely working as Software Engineer at Domestika. I am part of the area of Growth and eCommerce.
+      { new Date().getFullYear() - new Date(2012,3,7).getFullYear() } años de experiencia en desarrollo web.
     </p>
     <p>
-      Also, I was working as a Lead Developer at Bi4Group (Granada). I was in charge of some development tasks (programming, TDD, and code reviews) and management (applying agile methodologies such as Scrum or Kanban, customer support, and helping or coaching the team members).
+      { new Date(2020,12,31).getFullYear() - new Date(2016,4,1).getFullYear() } años de experiencia en dirección de equipos.
+    </p>
+    <StaticImage
+      src="../images/about.jpg"
+      width={300}
+      quality={95}
+      formats={["auto", "webp", "avif"]}
+      alt="about"
+      style={{ marginBottom: `1.45rem` }}
+    />
+    <p>
+      Actualmente me encuentro trabajando en remoto como Ingeniero Software en <a href="http://domestika.org/" target='_blank'>Domestika</a>. En el area de adquisición de usuarios y eCommerce de la plataforma.
     </p>
     <p>
-      Previously, I worked for two years as a web application developer in the company NGA-HR, based on the HR sector, where I participated in a project to exchange information between different payroll systems. In addition, I worked, almost 2 years, for Razorfish Healthware, a company belonging to the Publicis Group, in the PeopleWho project.
+      También he trabajado como Líder de equipo en <a href="https://nucleoo.com/" target='_blank'>Nucleoo</a> (Granada), realizando tareas de ingeniería (programación, TDD y revisiones de código) y de gestión (aplicando metodologías ágiles como Scrum o Kanban, soporte a cliente, y training a compañeros).
     </p>
-    <p></p>
-    <Link to="/">Go back to the homepage</Link>
+    <p>
+      Anteriormente trabajé, durante más de dos años, como Ingeniero Software (en desarrollo de aplicaciones web) para la compañía <a href="https://alight.com/ngahr" target='_blank'>NGA-HR (Alight)</a>, dedicada al sector de los RRHH donde participé de un proyecto propio de intercambio de información entre distintos sistemas de nóminas. Con mayor anterioridad trabajé también como programador web, durante casi 2 años, para Razorfish Healthware compañía perteneciente al Grupo Publicis, en el proyecto PersonasQue.
+    </p>
+    <Link to="/">Ir al inicio</Link>
   </Layout>
 )
 
